41286c70fe9d08e538bc360d9f0374aee7c771d3
escapevoat <escapevoat@gmail.com>
2015-08-20 20:17:41 -0700

    Initial Push

---
+++ js/scripts.js
@@ -0,0 +1,35 @@
+jQuery(function($) {
+
+	var container = document.querySelector('#grid');
+	var msnry = new Masonry( container, {
+		columnWidth: 200,
+		itemSelector: '.grid-item'
+	});
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+	$(document).ready(function() {
+
+
+
+
+	});
+
+
+
+
+
+
+
+});
