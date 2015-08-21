a9f05c9cc2bb8e95bc0b87506c7790b980727589
Josiah Rooney <jrooney@optirev.net>
2015-08-20 20:20:02 -0700

    Initial Commit

---
+++ front-page.php
@@ -0,0 +1,13 @@
+<?php get_header(); ?>
+<section id="content">
+	<?php if ( have_posts() ) : while ( have_posts() ) : the_post(); ?>
+		<article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
+			<section class="entry-content">
+				<?php if ( has_post_thumbnail() ) { the_post_thumbnail(); } ?>
+				<?php the_content(); ?>
+				<div class="entry-links"><?php wp_link_pages(); ?></div>
+			</section>
+			</article>
+	<?php endwhile; endif; ?>
+</section>
+<?php get_footer(); ?>
